1dfaf30354f460357daa4cbdc0455ea533e559f2
David Bauer <david.bauer@open-xchange.com>
2014-02-08 17:46:43 +0100

    Added default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,4 +53,6 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['concat', 'copy']);
 };
