d29a6bbb4b68a99bc095a1bd14ce1b234f9d2c32
Thomas Oberndörfer <toberndo@yarkon.de>
2014-06-14 15:53:10 +0200

    Change header comment to https

--- common/lib/defaults.js
+++ common/lib/defaults.js
@@ -38,7 +38,7 @@ define(function (require, exports, module) {
   }
 
   function init() {
-    model.setOpenPGPComment('Email security by Mailvelope - http://www.mailvelope.com');
+    model.setOpenPGPComment('Email security by Mailvelope - https://www.mailvelope.com');
     model.setOpenPGPVersion('Mailvelope ' + defaults.version);
     if (!model.getWatchList()) {
       model.setWatchList(defaults.watch_list);
