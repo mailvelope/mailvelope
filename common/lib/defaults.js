1efc4d1539e72b5f65496605aac5ec7c8b353ebd
Thomas Oberndörfer <toberndo@yarkon.de>
2014-09-24 13:31:06 +0200

    Merge default watchlist entries on update

--- common/lib/defaults.js
+++ common/lib/defaults.js
@@ -22,8 +22,6 @@ define(function (require, exports, module) {
 
   var defaults = mvelo.data.loadDefaults();
 
-  init();
-
   function randomColor() {
     return '#'+('00000'+(Math.random()*(1<<24)|0).toString(16)).toUpperCase().slice(-6);
   }
@@ -40,25 +38,82 @@ define(function (require, exports, module) {
   function init() {
     model.setOpenPGPComment('Email security by Mailvelope - https://www.mailvelope.com');
     model.setOpenPGPVersion('Mailvelope ' + defaults.version);
-    if (!model.getWatchList()) {
-      model.setWatchList(defaults.watch_list);
-    }
     var prefs = model.getPreferences();
     if (!prefs) {
       defaults.preferences.security.secure_color = randomColor();
       defaults.preferences.security.secure_code = randomString(3);
       model.setPreferences(defaults.preferences);
+      model.setWatchList(defaults.watch_list);
     } else {
       if (typeof prefs.main_active == 'undefined') {
         prefs.main_active = defaults.preferences.main_active;
+        model.setPreferences(prefs);
       }
-      model.setPreferences(prefs);
+      if (prefs.version !== defaults.version) {
+        prefs.version = defaults.version;
+        model.setPreferences(prefs);
+        mergeWatchlist(defaults);
+      }
     }
+  }
+
+  function mergeWatchlist(defaults) {
+    var mod = false;
+    var localList = model.getWatchList() || [];
+    defaults.watch_list.forEach(function(defaultSite) {
+      var localSite = localList.find(function(localSite) {
+        return localSite.site === defaultSite.site;
+      });
+      if (localSite) {
+        defaultSite.frames.forEach(function(defaultFrame) {
+          localSite.frames = localSite.frames || [];
+          var localFrame = localSite.frames.find(function(localFrame) {
+            return localFrame.frame === defaultFrame.frame;
+          });
+          if (!localFrame) {
+            localSite.frames.push(defaultFrame);
+            mod = true;
+          }
+        });
+      } else {
+        localList.push(defaultSite);
+        mod = true;
+      }
+    });
+    if (mod) {
+      model.setWatchList(localList);
+    }
+  }
+
+  // polyfill https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+  if (!Array.prototype.find) {
+    Array.prototype.find = function(predicate) {
+      if (this === null) {
+        throw new TypeError('Array.prototype.find called on null or undefined');
+      }
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+      var list = Object(this);
+      var length = list.length >>> 0;
+      var thisArg = arguments[1];
+      var value;
+
+      for (var i = 0; i < length; i++) {
+        value = list[i];
+        if (predicate.call(thisArg, value, i, list)) {
+          return value;
+        }
+      }
+      return undefined;
+    };
   }
 
   function getVersion() {
     return defaults.version;
   }
+
+  init();
 
   exports.getVersion = getVersion;
 
