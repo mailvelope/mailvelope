b46e0ee29c2f7218e37f0a737cf9d5bef9af6583
Leonard Brünings <leonard.bruenings@1und1.de>
2015-01-20 17:25:54 +0100

    possible fix fir deploy-gh-pages

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -12,6 +12,8 @@ mkdir out;
  git config user.name "Travis-CI"
  git config user.email "travis@travis-ci.org"
  cp -R ../build/doc/* .
- git commit -am "Deployed to Github Pages"
+ git add .
+ git commit -m "Deployed to Github Pages"
+ echo "Deploying to gh-pages now"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
