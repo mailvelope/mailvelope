b72681abdcf833cfce73bea9d8f6e8bbf0e6deb3
River MacLeod <river@mm0hai.net>
2015-02-09 18:45:57 +0100

    Limit size of key text to import.

--- common/ui/keyring/importKey.js
+++ common/ui/keyring/importKey.js
@@ -27,9 +27,11 @@ var mvelo = mvelo || null;
 
   var KEY_ID_REGEX = /^([0-9a-f]{8}|[0-9a-f]{16}|[0-9a-f]{40})$/i;
   var HKP_SERVER_BASE_URL = 'https://keyserver.ubuntu.com';
+  var MAX_KEY_IMPORT_SIZE = 10000000;
 
   options.registerL10nMessages([
     "key_import_error",
+    "key_import_too_big",
     "key_import_invalid_text",
     "key_import_exception",
     "alert_header_warning",
@@ -64,6 +66,10 @@ var mvelo = mvelo || null;
   function onImportKey(callback) {
     clearAlert();
     var keyText = $('#newKey').val();
+    if (keyText.length > MAX_KEY_IMPORT_SIZE) {
+      $('#importAlert').showAlert(options.l10n.key_import_error, options.l10n.key_import_too_big, 'danger', true);
+      return;
+    }
 
     // find all public and private keys in the textbox
     var publicKeys = keyText.match(publicKeyRegex);
@@ -130,9 +136,15 @@ var mvelo = mvelo || null;
   };
 
   function onChangeFile(event) {
+    clearAlert();
     var reader = new FileReader();
     var file = event.target.files[0];
     reader.onloadend = function(ev) { $('#newKey').val(ev.target.result); };
+
+    if (file.size > MAX_KEY_IMPORT_SIZE) {
+      $('#importAlert').showAlert(options.l10n.key_import_error, options.l10n.key_import_too_big, 'danger', true);
+      return;
+    }
     reader.readAsText(file);
   }
 
