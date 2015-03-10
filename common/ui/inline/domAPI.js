66088fa205b1725dffbb4d5bf5075008136267c7
Thomas Oberndörfer <thomas@mailvelope.com>
2015-03-10 18:36:20 +0100

    Set higher max logo size

--- common/ui/inline/domAPI.js
+++ common/ui/inline/domAPI.js
@@ -286,8 +286,8 @@ mvelo.domAPI.setLogo = function(keyringId, dataURL, revision, callback) {
     error.code = 'LOGO_INVALID';
     throw error;
   }
-  if (dataURL.length > 10 * 1024) {
-    error = new Error('Data URL exceeds 10KB limit.');
+  if (dataURL.length > 15360) {
+    error = new Error('Data URL string size exceeds 15KB limit.');
     error.code = 'LOGO_INVALID';
     throw error;
   }
