6e291d0a583fca4a361a29df2bd1f4794f97c967
Thomas Oberndörfer <thomas@mailvelope.com>
2015-07-02 18:28:57 +0200

    Deploy latest builds with stable naming

--- deploy-nightly.sh
+++ deploy-nightly.sh
@@ -5,5 +5,7 @@ if [ $TRAVIS_PULL_REQUEST == "false" ] && [ $TRAVIS_BRANCH == "dev" ]; then
   mkdir nightly
   cp dist/mailvelope.chrome.zip "nightly/mailvelope.$(date +%Y-%m-%d).chrome.zip"
   cp dist/mailvelope.firefox.xpi "nightly/mailvelope.$(date +%Y-%m-%d).firefox.xpi"
+  cp dist/mailvelope.chrome.zip nightly/mailvelope.latest.chrome.zip
+  cp dist/mailvelope.firefox.xpi nightly/mailvelope.latest.firefox.xpi
   sshpass -p ${DEPLOY_PASS} scp -r nightly/ ${DEPLOY_USER}@build.mailvelope.com:html/ 
 fi
