82456c814898cf1c345e0a6a3c7bb55a1b242788
Thomas Oberndörfer <thomas@mailvelope.com>
2018-01-15 14:37:12 +0530

    Fix editor unit tests

--- test/components/editor/editor-test.js
+++ test/components/editor/editor-test.js
@@ -1,22 +1,21 @@
 
-import $ from 'jquery';
-import * as editor from '../../../src/components/editor/editor';
+import Editor from '../../../src/components/editor/editor';
 
 describe('Editor UI unit tests', () => {
-  describe('checkEnvironment', () => {
-    beforeEach(() => {
-      sinon.stub($, 'parseQuerystring').returns({embedded: true, id: '12345'});
-    });
+  let editor;
 
-    afterEach(() => {
-      $.parseQuerystring.restore();
+  beforeEach(() => {
+    editor = new Editor({id: 'abc'});
+  });
+
+  describe('onSetInitData', () => {
+    beforeEach(() => {
+      sinon.stub(editor, 'setState');
     });
 
     it('should work', () => {
-      editor.checkEnvironment();
-      expect(editor.embedded).to.be.true;
-      expect(editor.id).to.equal('12345');
-      expect(editor.name).to.equal('editor-12345');
+      editor.onSetInitData({text: '123', signMsg: 'abc', primary: 'abc', privKeys: []});
+      expect(editor.setState.withArgs({defaultPlainText: '123', signMsg: true, signKey: 'abc', primaryKey: true, privKeys: []}).calledOnce).to.be.true;
     });
   });
 });
