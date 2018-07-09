38278de24ff1f16edc91561f7da61182000d631b
Remy Bertot <remy@passbolt.com>
2018-07-09 16:05:03 +0530

    Add support for img tag for self origin

--- src/controller/encryptedForm.controller.js
+++ src/controller/encryptedForm.controller.js
@@ -109,14 +109,14 @@ export default class EncryptedFormController extends sub.SubController {
   getCleanFormHtml(dirtyHtml) {
     return dompurify.sanitize(dirtyHtml, {
       ALLOWED_TAGS: [
-        'bdi', 'bdo', 'br', 'datalist', 'div', 'fieldset', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'input',
-        'label', 'legend', 'optgroup', 'option', 'p', 'select', 'small', 'span', 'strong', 'textarea'
+        'bdi', 'bdo', 'br', 'datalist', 'div', 'fieldset', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'img',
+        'input', 'label', 'legend', 'optgroup', 'option', 'p', 'select', 'small', 'span', 'strong', 'textarea'
       ],
       ALLOWED_ATTR: [
         // default html attributes
         'accesskey', 'class', 'dir', 'hidden', 'id', 'lang', 'tabindex', 'title', 'action', 'name', 'alt',
         'checked', 'dirname', 'disabled', 'for', 'required', 'list', 'max', 'maxlength', 'min', 'multiple',
-        'name', 'pattern', 'placeholder', 'readonly', 'required', 'size', 'step', 'type', 'value',
+        'name', 'pattern', 'placeholder', 'readonly', 'required', 'src', 'size', 'step', 'type', 'value',
         // custom data attributes
         'data-action', 'data-recipient'
       ],
