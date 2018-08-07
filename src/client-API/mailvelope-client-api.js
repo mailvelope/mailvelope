e5ca070b8e121b32b47e893c8bc59fd3eba02a58
Remy Bertot <remy@passbolt.com>
2018-08-07 16:04:00 +0200

    Fix show an error when customElements is not defined

--- src/client-API/mailvelope-client-api.js
+++ src/client-API/mailvelope-client-api.js
@@ -683,48 +683,52 @@
   window.setTimeout(() => {
     window.dispatchEvent(new CustomEvent('mailvelope', {detail: window.mailvelope}));
   }, 1);
+
+  /**
+   * OpenPGPEncryptedForm custom HTMLElement
+   */
+  class OpenPGPEncryptedForm extends HTMLElement {
+    // Invoked when the custom element is first connected to the document's DOM.
+    connectedCallback() {
+      this.dispatchEvent(new Event('connected'));
+      const id = this.getAttribute('id');
+      if (!id) {
+        const error = new Error('No form id for openpgp-encrypted-tag. Please add a unique identifier.');
+        error.code = 'NO_FORM_ID';
+        return this.onError(error);
+      }
+      let html;
+      const scriptTags = this.getElementsByTagName('script');
+      if (scriptTags.length) {
+        html = scriptTags[0].innerText;
+      } else {
+        const error = new Error('No form template for openpgp-encrypted-tag. Please add a form template.');
+        error.code = 'NO_FORM_SCRIPT';
+        return this.onError(error);
+      }
+      window.mailvelope.createEncryptedFormContainer(`#${id}`, html, this.getAttribute('signature'))
+      .then(data => this.onEncrypt(data), error => this.onError(error));
+    }
+
+    onEncrypt(data) {
+      this.dispatchEvent(new CustomEvent('encrypt', {
+        detail: {armoredData: data.armoredData},
+        bubbles: true,
+        cancelable: true
+      }));
+    }
+
+    onError(error) {
+      this.dispatchEvent(new ErrorEvent('error', {
+        message: error.message,
+        error
+      }));
+    }
+  }
+
+  // See. https://developer.mozilla.org/en-US/docs/Web/API/Window/customElements#Specification#Browser_compatibility
+  if (typeof window.customElements !== 'undefined') {
+    window.customElements.define('openpgp-encrypted-form', OpenPGPEncryptedForm);
+  }
+
 }());
-
-/**
- * OpenPGPEncryptedForm custom HTMLElement
- */
-class OpenPGPEncryptedForm extends HTMLElement {
-  // Invoked when the custom element is first connected to the document's DOM.
-  connectedCallback() {
-    this.dispatchEvent(new Event('connected'));
-    const id = this.getAttribute('id');
-    if (!id) {
-      const error = new Error('No form id for openpgp-encrypted-tag. Please add a unique identifier.');
-      error.code = 'NO_FORM_ID';
-      return this.onError(error);
-    }
-    let html;
-    const scriptTags = this.getElementsByTagName('script');
-    if (scriptTags.length) {
-      html = scriptTags[0].innerText;
-    } else {
-      const error = new Error('No form template for openpgp-encrypted-tag. Please add a form template.');
-      error.code = 'NO_FORM_SCRIPT';
-      return this.onError(error);
-    }
-    window.mailvelope.createEncryptedFormContainer(`#${id}`, html, this.getAttribute('signature'))
-    .then(data => this.onEncrypt(data), error => this.onError(error));
-  }
-
-  onEncrypt(data) {
-    this.dispatchEvent(new CustomEvent('encrypt', {
-      detail: {armoredData: data.armoredData},
-      bubbles: true,
-      cancelable: true
-    }));
-  }
-
-  onError(error) {
-    this.dispatchEvent(new ErrorEvent('error', {
-      message: error.message,
-      error
-    }));
-  }
-}
-
-window.customElements.define('openpgp-encrypted-form', OpenPGPEncryptedForm);
