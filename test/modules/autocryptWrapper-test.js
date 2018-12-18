5e9b883ff3c528ab8efac34d55e7f82e7a1037fd
Azul <azul@riseup.net>
2018-12-18 14:15:02 +0100

    autocrypt: compare addr part of header to plain email
    Also catching Error('Invalid Autocrypt Header: no valid header found').
    If there is no autocrypt header at all that should not cause an error.

--- test/modules/autocryptWrapper-test.js
+++ test/modules/autocryptWrapper-test.js
@@ -11,6 +11,12 @@ describe('Test basic autocrypt wrapper functionality', () => {
       storage = new LocalStorageStub();
       autocryptWrapper.default.__Rewire__('mvelo', {storage});
     });
+
+    afterEach(() => {
+      /* eslint-disable-next-line no-undef */
+      __rewire_reset_all__();
+    });
+
     it('parses and stores the key', async () => {
       const addr = 'test@mailvelope.com';
       const keydata = 'base64';
@@ -21,9 +27,15 @@ describe('Test basic autocrypt wrapper functionality', () => {
       expect(result).to.equal(keydata.replace(/\s+/g, ''));
     });
 
-    afterEach(() => {
-      /* eslint-disable-next-line no-undef */
-      __rewire_reset_all__();
+    it('handles adresses with names', async () => {
+      const addr = 'test@mailvelope.com';
+      const keydata = 'base64';
+      const header = Autocrypt.stringify({keydata, addr});
+      const date = Date.now().toString();
+      const fromHeader = `name goes here <${addr}>`;
+      await autocryptWrapper.processHeader(header, fromHeader, date, 'id2');
+      const result = await autocryptWrapper.lookup(addr, 'id2');
+      expect(result).to.equal(keydata.replace(/\s+/g, ''));
     });
 
     it('stores the keys separately per identity', async () => {
