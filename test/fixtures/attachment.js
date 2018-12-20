4116d6fb2edcac1d49c0f3157b4c0ebe3bd304e5
Thomas Oberndörfer <thomas@mailvelope.com>
2018-12-20 16:51:36 +0100

    Remove unused fixtures

--- test/fixtures/attachment.js
+++
@@ -1,6 +0,0 @@
-export default {
-  'type': 'attachment',
-  'content': 'data:text/plain;base64,VGhpcyBpcyB0b3Agc2VjcmV0IQ==',
-  'mimeType': 'text/plain',
-  'filename': 'secret_attachment.txt'
-};
