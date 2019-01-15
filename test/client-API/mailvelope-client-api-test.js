4e54826d720e251f47882e31bd89249c372509ba
Azul <azul@riseup.net>
2019-01-15 08:12:28 +0100

    describe subsections in api test

--- test/client-API/mailvelope-client-api-test.js
+++ test/client-API/mailvelope-client-api-test.js
@@ -10,29 +10,32 @@ main.initController();
 
 /* global mailvelope */
 describe('Mailvelope Client API', () => {
-
-  let existing_keyring;
-
-  beforeEach(async () => {
-    mvelo.storage = new LocalStorageStub();
-    keyring.init();
-    existing_keyring = await mailvelope.createKeyring('existing@test.example');
+  describe('Versioning', () => {
+    it('reports its version', () => {
+      api.default.__Rewire__('prefs', {version: '1.2.3-dev'});
+      return expect(mailvelope.getVersion()).to.eventually.equal('1.2.3');
+    });
   });
 
-  it('reports its version', () => {
-    api.default.__Rewire__('prefs', {version: '1.2.3-dev'});
-    return expect(mailvelope.getVersion()).to.eventually.equal('1.2.3');
+  describe('Handling keyrings', () => {
+    let existing_keyring;
+
+    beforeEach(async () => {
+      mvelo.storage = new LocalStorageStub();
+      keyring.init();
+      existing_keyring = await mailvelope.createKeyring('existing@test.example');
+    });
+
+    it('can create a keyring', () =>
+      expect(mailvelope.createKeyring('email@test.example')).to.eventually.be.ok);
+
+    it('rejects creating duplicate keyring', () =>
+      expect(mailvelope.createKeyring('existing@test.example')).to.eventually.be.rejected);
+
+    it('can get a keyring', () =>
+      expect(mailvelope.getKeyring('existing@test.example')).to.become(existing_keyring));
+
+    it('rejects getting missing keyring', () =>
+      expect(mailvelope.getKeyring('email@test.example')).to.be.rejected);
   });
-
-  it('can create a keyring', () =>
-    expect(mailvelope.createKeyring('email@test.example')).to.eventually.be.ok);
-
-  it('rejects creating duplicate keyring', () =>
-    expect(mailvelope.createKeyring('existing@test.example')).to.eventually.be.rejected);
-
-  it('can get a keyring', () =>
-    expect(mailvelope.getKeyring('existing@test.example')).to.become(existing_keyring));
-
-  it('rejects getting missing keyring', () =>
-    expect(mailvelope.getKeyring('email@test.example')).to.be.rejected);
 });
