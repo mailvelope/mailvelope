77313311eaaa17c1fd245d88189bace9131e6630
Azul <azul@riseup.net>
2019-01-16 10:35:00 +0100

    autocrypt: make Store a private class

--- src/modules/autocryptWrapper.js
+++ src/modules/autocryptWrapper.js
@@ -1,5 +1,10 @@
+/**
+ * Copyright (C) 2015-2018 Mailvelope GmbH
+ * Licensed under the GNU Affero General Public License version 3
+ */
+
+import mvelo from '../lib/lib-mvelo';
 import Autocrypt from 'autocrypt';
-import Store from '../lib/Store';
 import {goog} from './closure-library/closure/goog/emailaddress';
 
 export const name = 'AC';
@@ -72,4 +77,45 @@ function armor(base64) {
   const footer = '-----END PGP PUBLIC KEY BLOCK-----';
   const lines = base64.match(/.{1,64}/g);
   return [head, ''].concat(lines).concat([footer]).join('\n');
+}
+
+
+class Store {
+  constructor(storageKey) {
+    this.storageKey = storageKey;
+    this.map = new Map();
+  }
+
+  async init() {
+    if (this.initalized) {
+      return;
+    }
+    const attributes = await mvelo.storage.get(this.storageKey);
+    if (attributes) {
+      Object.keys(attributes).forEach(key => this.map.set(key, attributes[key]));
+    }
+    this.initialized = true;
+  }
+
+  get(key, cb) {
+    this.init()
+    .then(cb(undefined, this.map.get(key)));
+  }
+
+  put(key, val, cb) {
+    this.init()
+    .then(this.map.set(key, val))
+    .then(this.store())
+    .then(cb());
+  }
+
+  async store() {
+    await mvelo.storage.set(this.storageKey, this.toObject());
+  }
+
+  toObject() {
+    const all = {};
+    this.map.forEach((value, key) => all[key] = value);
+    return all;
+  }
 }
