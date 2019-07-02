90e4fac3a18c9be5c91b8b090a4acc90d762ceb2
Martin Hauck <martin@mailvelope.com>
2019-07-02 10:49:24 +0200

    Redesign of spinner

--- src/components/util/Spinner.js
+++ src/components/util/Spinner.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Spinner.css';
+import './Spinner.scss';
 
 export default class Spinner extends React.Component {
   constructor(props) {
@@ -30,7 +30,7 @@ export default class Spinner extends React.Component {
     return (
       <div className={`m-spinner-${this.props.fullscreen ? 'fullscreen' : 'inline'} ${this.state.hide ? 'hide' : ''}`} style={this.props.style}>
         <div className="symbol">
-          <div className="bounce1"></div><div className="bounce2"></div><div className="bounce3"></div>
+          <div className="bounce1"></div><div className="bounce2"></div>
         </div>
       </div>
     );
