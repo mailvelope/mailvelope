mailvelope/mailvelope

commit 4b644d8dd0470e36e6c953286a363abc08cfadcc

Fix filtering User IDs
Previously User Attributese (such as photos, but not limited to them)
would trigger an error because `user.userId` attribute is `null` in that
case (`user.userAttribute` property would be set).

The fix checks if an `userId` property is set before using it
effectively omitting User Attributes.

See: https://openpgpjs.org/openpgpjs/doc/key.js.html#line800

src/modules/key.js
@@ -324,10 +324,12 @@ export function filterUserIdsByEmail(key, email) {
   const keptUsers = [];
 
   for (const user of key.users) {
-    const userMapped = {userId: user.userId.userid};
-    mapKeyUserIds(userMapped);
-    if (userMapped.email.toLowerCase() === email.toLowerCase()) {
-      keptUsers.push(user);
+    if (user.userId) {
+      const userMapped = {userId: user.userId.userid};
+      mapKeyUserIds(userMapped);
+      if (userMapped.email.toLowerCase() === email.toLowerCase()) {
+        keptUsers.push(user);
+      }
     }
   }
 

commit 486ba72a3b6a3b6590626a2eb08fc56f4d0538c2

Add test to show regression from commit 4b644d8
This test illustrates error when encountering key with a User Attribute
(e.g. a photo).

See: #613

test/fixtures/keys.js
@@ -6,6 +6,7 @@ import johnd_pub from './keys/john_doe_pub.asc';
 import johnd_prv from './keys/john_doe_prv.asc';
 import gordonf_pub from './keys/gordon_freeman_pub.asc';
 import maxp_pub from './keys/max_payne_pub.asc';
+import wiktor_pub from './keys/wiktor_pub.asc';
 
 export default {
   api_test_pub,
@@ -15,5 +16,6 @@ export default {
   johnd_pub,
   johnd_prv,
   gordonf_pub,
-  maxp_pub
+  maxp_pub,
+  wiktor_pub
 };

test/fixtures/keys/wiktor_pub.asc
@@ -1,0 +1,154 @@
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBFhoYHoBEADzmg9UuwDrtvyejU01gDY1J1iJiCi4XGJ4lCfYeLC2jSagIxU/
+5Lu0lRft0Loi2tsjpo0c8docP7HFxafEEvnnt/iabd6I536llMuw0uno4PgnD3lj
+cCMZLT+vn+amIDtalzVoMnSqzoNUotMNMtjIFuAaQ/wr4/Mp9CIgJdviGUc3Pscq
+UiiUVVtk6uF0x657NULZgSIT/Mrqlr2i4RuyPwXe2Qt0uEA3KWWjF0l2NpAMVrqz
++nHsLoNOaAsfdx94bzKQrrSeSQqEO2f+/eO/hbUAFAmEhrotmUO8wJNygo8Tgkdl
+zFI+UE4p8/KW0aCgGGgR8YkCvHq2OQhAAYFNJoNzHqw0FGxdsY8qWFkYpoSB8zKs
+pNy8KliofCamMYXoPF7eVIxIiKvxrAykGP4jNnzSoV0cn+bYfXnox1IhnqbnoJIT
+7kTmXv4JmWoYm8ThHqpEgcQOUUQzSRXb9OiNwiXT71ijeO1qswMRpsgk6AGKSZGW
+xa3c4ive/p8z1Ax27BFZSh2FceIcMCcGLrDjnQYgeFsAJ1jSxZQXkGuJFHfb4nff
+Big7aq/vyKrQFQXG0NQQL7rZAdk/s665vifos0yPmRDu7yDT1ggdyBp4Pa4re+ZJ
+cNRNzNHozU9al+CoImCQjnTtKMXmOe/BzGrpHI4QR3NNzVa423WCIWkHfwARAQAB
+tClXaWt0b3IgS3dhcGlzaWV3aWN6IDx3aWt0b3JAbWV0YWNvZGUuYml6PokELgQT
+AQoCGAIbAQgLCQgHDQwLCgUVCgkICwIeAQIXgAUJBaLXyhYhBGU5CaLw43wQb1+v
+VGyIV+DY6PB0BQJc2r1oNBSAAAAAABIAGXByb29mQG1ldGFjb2RlLmJpemRuczpt
+ZXRhY29kZS5iaXo/dHlwZT1UWFRcFIAAAAAAEgBBcHJvb2ZAbWV0YWNvZGUuYml6
+aHR0cHM6Ly9naXN0LmdpdGh1Yi5jb20vd2lrdG9yLWsvMzg5ZDU4OWRkMTkyNTBl
+MWY5YTQyYmMzZDVkNDBjMTZVFIAAAAAAEgA6cHJvb2ZAbWV0YWNvZGUuYml6aHR0
+cHM6Ly93d3cucmVkZGl0LmNvbS91c2VyL3dpa3Rvci1rL2NvbW1lbnRzL2JvNW9p
+aC90ZXN0L3MUgAAAAAAqAEB0aW1lc3RhbXArYml0Y29pbi10cmFuc2FjdGlvbkBt
+ZXRhY29kZS5iaXphZmNiMDkyYzVjYTY0MDk1MjZkMThhZTljZjIyZDNiNTVkMzdl
+NzIzZWIxYjc0ZTNmODRmN2U2YjA1MmExNjJhSBSAAAAAABIALXByb29mQG1ldGFj
+b2RlLmJpemh0dHBzOi8vbmV3cy55Y29tYmluYXRvci5jb20vdXNlcj9pZD13aWt0
+b3ItazcUgAAAAAASABxwcm9vZkBtZXRhY29kZS5iaXpodHRwczovL21ldGFjb2Rl
+LmJpei9Ad2lrdG9yAAoJEGyIV+DY6PB0N+wQAM2qWCLGAC7eWEQIq9Zw/mjlztS+
+O/7xG9o8dbCBO/qVanDDZzCredeIHdKuo6llyKmPl+/QrPEpHbsl2r3uOQL2jIT8
+jHGjk+2LnoXMS1BC6gUZlUv3AgRiIf/fVyYHchwpAViB8XQnXPSA6UMC4fHMVpXo
+21JhklY2v+fW39wlSaCR2K4T6MBWy2AmgvENYTn8yeneqiIldpD1k6ZLzOmFL0nM
+ISd37XxeQ4rdE8bJqbiYuMJikuWu5tgnMc0to8U8mn2waBYPd/rf2jrAEefvQO/2
+Rt9cLrbauaBln8EL5Y49WsmAsYKRcRWbkTVDnuEXDhC2utxOG1n/kAZunu87iRwB
+KgaY6N2y5nizir5yJiNiNH1IAlKvS87rSUYu4A6wrZki1cnvLdtmSsXDlagiKmWV
+qpFWqbLrfPJ/8bF8ql/yPte7q2F9J0AE0C0eFzuOUYlUslxH6JHf8EmLmQVBa6/z
+4R7I4MdbPpgfMrtkj9HC/PZUTbvkplyhDFszd9J6cPuMPRQDufzXn6KyjrkwqMPY
+WefG+ynHxf93eC1ZJ6niU/SkZcI9nWWtsIq1WjmmlmegsTvWH08Lvllv84m7r5f8
+bBZeOmeo5YJ1y10qk3WR2G6OQ7ygpBRo+cu55lpF9s6zNZE+ihu5YLgJsVsHL9Dv
+LCCBRo/37oQH+n4e0VVUZW9wZW5wZ3BpZCtjb29raWU6QGh0dHBzOi8vZ2lzdC5n
+aXRodWIuY29tL3dpa3Rvci1rLzM4OWQ1ODlkZDE5MjUwZTFmOWE0MmJjM2Q1ZDQw
+YzE2iQO6BBMBCgGkAhsBCAsJCAcNDAsKBRUKCQgLAh4BAheABQkFotfKFiEEZTkJ
+ovDjfBBvX69UbIhX4Njo8HQFAlzavWg0FIAAAAAAEgAZcHJvb2ZAbWV0YWNvZGUu
+Yml6ZG5zOm1ldGFjb2RlLmJpej90eXBlPVRYVFwUgAAAAAASAEFwcm9vZkBtZXRh
+Y29kZS5iaXpodHRwczovL2dpc3QuZ2l0aHViLmNvbS93aWt0b3Itay8zODlkNTg5
+ZGQxOTI1MGUxZjlhNDJiYzNkNWQ0MGMxNlUUgAAAAAASADpwcm9vZkBtZXRhY29k
+ZS5iaXpodHRwczovL3d3dy5yZWRkaXQuY29tL3VzZXIvd2lrdG9yLWsvY29tbWVu
+dHMvYm81b2loL3Rlc3QvSBSAAAAAABIALXByb29mQG1ldGFjb2RlLmJpemh0dHBz
+Oi8vbmV3cy55Y29tYmluYXRvci5jb20vdXNlcj9pZD13aWt0b3ItazcUgAAAAAAS
+ABxwcm9vZkBtZXRhY29kZS5iaXpodHRwczovL21ldGFjb2RlLmJpei9Ad2lrdG9y
+AAoJEGyIV+DY6PB05/sQAOYu11/A+2iivH9tkVpDX+O6TL/wOkz+86qPWeWO1y9G
+C51fwxaZJDLOJ2Wo3HWW9U15cBSObY8F/jysb9XgxZHHffzj+59FaRrBDMOuYGxe
+AECQrAm7wJ176Rfpgh8FsvIgrGYO41IbzHH/DAyC6AysrZGv1YFf60tEI9ldbm7F
+Cz3l91Q/PjPrMOpyl1/NX5yKj9/7uhNbzWPV16YNJOi0ZEAtb52z70GLRrNVUzGC
+1TrHvHZx2PRrGfeFpjODcLs0Z4P4J361aL/oKZwNPlXITZuMIyB01bulX+oW/+Ou
+wfrO/IFJLqSSL+2y69n8CWdne7jMRmOE0fHm34vLZb20eyHLXekuh+C7r6hIb5cY
+E1aJuQNy7y6wAUEqrm29AiAk6UaQ6L81ahLcdpQqnLJBU1IkejzxrqIpQW2jRzHY
+F+sTed1Q6oO3kJawcQRXZBkEUnWL6ovVfw/UYmADDVV8QNrdJmtWSRk7WcXIM/K7
+dkcQ36Pfb/sjylmcPbcyOJPBESmVqOcD3TG5u+lfUkAw4XZ0SP+0vNU3P4n1vckD
+NKtjEYKwc9PyIJhNRQGOziD+GuhZSCu+qX9ff+LPcBDXwDvfLDWhgGFsatEzOW7G
+Qngdg/nINDT3KXrrpEUFaDQJSRvSBho4HMYBI6PurZctjxsVFygiqb0lG+OqZYUD
+uQINBFnnYPUBEADLT1mqVQ6zF+jD4T9junpKDxvgvrMPQfz3owXjugNfWeCku4/K
+AjRAxtiJfihCv9Sh1ocrk3YwxGSniYPNDdl2vRaRUrYeW9Vxl8CajueWuK+LzSgP
+oK/eZOIAm3bD6lSHUNlzGU0U/8VLlpb+VcdvRJOaDvXO5q1M0C2NM3qtkxRVp6vW
+HgI6wat8we54goHHdYXlgXXSWWk6Abwbu8jnGpJqLGHzD579Ih2K1FY3zN/w9CgR
+PyF7nfzETSVvudNwQN5w+lakvIHtwv0wwAwoieywAxT4v/hydXYiyQR81YC6ToQk
+iRpBSJa8tk6rYwIgCmdD5XHCFOdTfTJ9AcpIit2XHdwU2slaC5OmG53ITTZW7j2w
+/z6EZm8scNxCFpWo4DQMDQ6hHmzMNzD+7TDWZOobP75O7iViI01L+qoI++zS2sKA
+4KrOJp8MUk09YS+C1wEObx3OgKe33D8hfqGlPIsjduVkHIPXStD42709NjG6KGCt
+XlgyqWoXur/A3b1P6fD9oO8PA1AgX1nY9+jrQ4GJbVSgwGo4r3g0A3pcJ7Y27S6+
+kmsW3YTvDYtjVJ0TFuf92/p4GyMKhxGjA3oo5Zbdu3D0NTAzBIlOAHXd//PuQsqZ
+d75MNXp+Tksc7af40rL7JmyLt4LmH3Lpe1Xd4OT5cHAGfAZMq2KY05KS9QARAQAB
+iQRbBBgBCgAmAhsCFiEEZTkJovDjfBBvX69UbIhX4Njo8HQFAlu1988FCQQj100C
+KcFdIAQZAQoABgUCWedg9QAKCRC5eh7gnbQX7NPuD/0eYCqdetIgwnSw01UG88ZX
+xLnwtq2JY9C8sgWVjHRPB8e7e5w4DO4xWuh7XtpZt7Pb4iGjlVuyeo5wzA27BYqx
+znSPWMeYAnVVlY99vpZ8kwFOrIpPgM+735uf3+Z/cina+8NV/dU82gHaRWSbPxwM
+mitpegPlWOORRzgTlBNkJBOxdSCE11IrNq+8LjER6M538tt/d4qB7Dk4jA/mIvWT
+JsP6A7rz4FG990oDOkR3gnr+4ePVqMIxN1RKcHAnoDVYbHkg9VyzFbuZ7tLR54iV
+LzhnH6QqnOBJFPXJrLPbOtxE9jvjy22DeB6Q587dTX4neH3b6lpXu/pk9HP+YFqY
+1H4Y2ZugrXhZa2HZSBTybno+XwagYR/8BieXgKbckZQhQ8rUeliOwkeh7/7H1XPU
+E9xhhYdoEO49hN3EWYvFZMsnArKfIqD8itnl556m85mzQ5V2h2OXGYwl8gZlEah6
+B5sKZCPNxally+HNhiO6xnpJdlSv8mVZQVwi87rlqsKKwFMBCR9NlEokWs9jgSQc
+ySjJ/0sgnDRF337CG1BUEknQUmbVF9MNIKSC1MRupcuEgE6T1l1CduFRcdlvAHcm
+rzGJfUSA6LKpn3T+cOQu62XUcSiQBe22UoK3DQHkpBYpJ+FEBIjqQGw4mTi8jLZj
+27NN0tdN0AKU5bPnvG5lCQkQbIhX4Njo8HS31w/+InHhsN6DfHdgNybAKZ5Fk3Jq
+7e6T1v3a/wQ9KMAI5SY8IrJqYLf9JNHk8HHEeGm9jeblXYdY4rjgaJeAei9YyXmx
+0WZbe0L10VZeYcpzVvJQwLKuLljhYP5+kwy7JbfN6iD2HP6NRZLObX+R19BXBEMe
++6ORhQ+Oat8/bmxvakk9i2r7/YQMTjd702zB89KP6alO21sOfZarYhKpt50IEcBH
+OnDoByEg9yGJME51fFYyw2+FS0F5V6fIXcABiF5g+VsiRmIW5pOWHxmn82UEdEZL
+vyToM6p7cCNEVSoB0VlxrMav6yDyR+aNkI56ZUzFU0+dM6f4njqTR01F9u6vWx/4
+r6M4d8wHo5YEtBzV3sXm8MbkHLel13H6Gs7ITl7Fa66pgcOK9bjtqdRwm2NJpcDX
+pNvCEN4+PNblFk/eG5Hw56dUnmOAg5e50cQKV3YRJTVgBEe5lZxEe+mBruzS9M1f
+f0mvPleWt76Z9VhGS+prh2d9iuOaX7qI/8y6ZE5p59V/EqPEL/eWfoCT+Is9R1BF
+vaqhWb7BHk0uoUaBxP9UmRupHlbD5EbXVZYMAN2CTpmjdN2bUM9BKqPRXYcZIHzN
+4fLMGZMX86bI4A7Onv9cosxzchp0EgKI8Xb7VXR4Tu7JlLeUWAo05loJAodkl1QV
+4b5WAMrXOF8JbpNeqHu5AQ0EWz+VLQEIALmhfeveW4XtzMwXxQXEnPnle681Gzi0
+SOgfOpyqh7j5EnJ80O2ymB69girVZMrD8fdlix3bBmmQzcj4Emf154dkqzvupdBk
+Y3k8EHrmtmguMzH6odrrAfiUhgxhbt4H0ccyxP/kTcgsFJHNkjAmKZ39B0gwpKsn
+mY8yQB3737GhvnMoc7kvmltC0lIZLSbXv+EDSQJMsjzzmEu1Pcmwj8pbC4BgOtPC
+xrLOH+Eb82B0DcjzGWtAIsLQQUmjEeQ320LJd4o+Wsg9jIwHWMdOjWI1XQJAamT7
+N/oMD/JIZ8nF3fSSwlnzLmrEPjfNLRh99gwLTUJ66o+gZnYXL1XGE4MAEQEAAYkC
+PAQYAQoAJgIbDBYhBGU5CaLw43wQb1+vVGyIV+DY6PB0BQJbtffPBQkCy6MVAAoJ
+EGyIV+DY6PB0KXEQAL7DkLD9AtgrAUrwTUUYNQLN7vljMk5gTONfStUdVskGQj6h
+ImkOuUwZwyF0grJtcNuYDBQY4LzrfXrxB6MCTLF/WNJEoFwjHjOgB2kp5LaZ8U4g
+EW0lb79HOQjBiLiwUfiOskhfWGMz/OsvXU8nXppGWECZECZXzvGzrdk3XlEqfL7B
+qAHUrgpqs2gQLhsJZF7OzO1QqbUrW4xaoF+cMF+gIyC9Cu7aDSDBmV7mQRRRUIYx
+Zonvq/HrOwar7s3e26Xcpx7ZQw6KIH6r1By7kbkaQWTZDe9HbcnARdpYEGWMq6g4
+0lCUIVnQFCgGBXY598HKwH03fxssoLFAbQ6SmaWg2YHWKD7kbfzXhxK+hu9UgwkA
+RFlpujMxNvdCvNlAFMvVZ7srkNMvkOdJ0lD/ZpL1z7Rk2NQph2Yua+7JEWJHdesg
+hG5CSHI6Zm2iL1kr6kU5xqi65IJwXwtDN21QuNBuZtz+ljqvCOnTy5OyBjK95rqP
+f0QF4w8M6Zj4X1Wm8/9t5PsSikwt2k5TLwXHrecT7XzAemg0JTj8ljgPnk3vxAH6
+bvzNJ+LE5gRQ1H1FDZ5WBbCrz7EWe0c3om9Grmf69ICggawfWm53HpmskRfciZpA
+SEoE/Eq2eLpJgbIAroExJOCpiN+X+RT4KsSAIapBQe+P2rrZhkJxdD+iDMwDuQEN
+BFs/lWoBCAC382iALtqa4BBPppi6XeWdqPsOaJm8sb31AGhM1FpCMmlsp3cy+7Sl
+9qH4adBCJzE47cY649VZ5xNUwsEoMVfwvxZZOrY0WHChlXnkwFxWBWw7tr1IdYtT
+DOux8oeH0b7vtQ2rFSV2QpNEiOzQZIKK+lVcnbbYXidg/RSVixRBw2j1hLmby9fD
+BsbKA9HOlavlzkGHr/1WYIfdZnk7XoFGo2J604FupUoiuN3Dm4v1JkUthbg6Sws5
+fvQ+8zOf1zQFYhRVIDAwlgbCWQ37EWJEpmR+HX5otzgKYRkay232miocJkBRRWeF
+OCPOF5ezBiBSkdjG2r9d5fYTm+Qsr4kNABEBAAGJA3IEGAEKACYCGwIWIQRlOQmi
+8ON8EG9fr1RsiFfg2OjwdAUCW7X30AUJAsui2AFAwHQgBBkBCgAdFiEE7x7g+pQg
++AT978Aml/3vNNq4+CsFAls/lWoACgkQl/3vNNq4+Cu52ggAhHEGJJtl6i7jaSPV
+ws2nRNvCj0/QZxCbc6Osy3o2HCRKY5m1jOefr5MmjEdWp8awXA5eB+0krbnMrCxS
+kZXungZ3CthGf6scUqSuyHZcqOJVa1VQPlIP9wYGAzp0yCGpCtrMtuYHwBWrN6Zr
+Jhf273Dk0zExV7hm7N7XZ06AB/TC/5E9dXHGfSZViXE5aOKh3pUufeDbhpO/5s3q
+GZyyMYlGVCkEDQNWxKn3BjGUwJ21pOHMJ85JOhA5eZgwmZeZ3d5KgAk1BGVIBGWs
+/nKyIxuBLTwS6Rsv+G+zqZrJ8oDbZE7muMJCobM3gFSpLDhEzFBBA7P8E7+wpjtF
+0gJG1gkQbIhX4Njo8HTltQ/7BnSXM8nUQ3Yi8tPQHCU4cXHp/2K5ABKbH6Fk3gYW
+ARSQcgGp7oTMeA1o8kufgGJrFxShfQOMYmlYL6/c/DVSMSEr/8SzwKcja3yf9BfA
+p2IlrZ/u3MuFWtNIGmoRmzo35/xvb2xP+nQ7b5unwDG14cGcWkOHpmTmuLwMRPts
+rjFh1RDlCHAevyYQvkqAmx1zPfowxXVSWCxwnoiDxM+AVDRTDnVbWOKWKWoefGTx
+Mv44yCz3cZh9LeioRkkcMy/f3+1hVs17jtsYu3QCw/K/DUmkCpwMQvHCLZP690ZC
+MQdxvT4QZnwzdImgWSqxK5NyLaR6Is0lu5/OJM1zgxEob09tBJoeQifLUayB0YEb
++tYRgbSt8rElKpZcPzvENl/edm5Pu2Ma+/c/X23qRJn/Rr0cCFnP9+6U//2++FLy
+Vv6sYT0kIo/yK2vez4yxCaQdN6nU9c+/8Mu+hCP26LZXcLuYDvDAGjlBFWuDlBh7
+pziXZ1QfEB4g/+1nCMlfTjJ10JjJcxcDQgHW6IGhkIQMkZEsma9oUe9Y4UopO/Of
+WNvfw3SykMcHYtc11Et502/vwpcRig/A+OY6KoLpAeSSA5qcHsGSx5uOLQcXCc0B
+3CWiMcgS5EYIre0WpW9pipukj+J8qrNvuS4c8VBAyV0bo0cf+H21NUhanfgXlwfo
+0Ze5AQ0EWz+VlgEIALnRfrt31BdFVOJWBWG4o7cJsO7vrJXzkKnsJHAFWa/mfARz
+MXB5P+nld1MBGOPbk+JBG/RgwUgR2uHssMH+VKAByggmm18dJInPB3q0IzJe2C6O
+1vPjugaGZkDMq9CXzFvGVmeGPG322O32c9T4/XAgElTto6rH2TiRfbmY/kdd4VnW
+o3M963CH2HlDMH/p/bqK7VTTTgN1HZ/buQGfNQ279tQ8tQjmGIVLFJeXo7Lf/Lay
+sJTJCJS0MMPzj3w+I745lY1rPzG9k3aXIkuPLd0NZA/nLUZoQ3YmTlN0DE/8399c
+gj+9m5EwUAuFa6m4ujK11QvuHbt+5tKcDqspsmcAEQEAAYkCPAQYAQoAJgIbIBYh
+BGU5CaLw43wQb1+vVGyIV+DY6PB0BQJbtffQBQkCy6KsAAoJEGyIV+DY6PB0JtEQ
+ALy1mBcDvWY1r2TOEr6nO9T3cKNe/0a5rZrIcBWWPsUP8vLxv3/ctpiRv636QAZ1
+wERiza70H1aGjXkKPdsLxLPdewDgY7dQc0m1y5jLnp0JIKi7U5XpzJ/pzCXC+RTc
+s/Tn5PqYbxUH3Ld3oQ0MzeMHfHqaow87zZOBSU+1dvPLfbYNwG0qh8CULhHZsOZ0
+LBFNbvsTt9WlED1khMx0cFCm3LkBSo+6G26xCDQ0b1Ql3+1WLD4eIddqGUlMR5za
+ZApNP8an/Tj1LKwGqA1walNARmrOk0sAs8lPlCBz3Ae4CqaDakM8Fp1IjhUJ1R3a
+JtJTcdLBZMBu56pPOHPB1QkxL8JIx/uHgKM0eKCTVEx/jFvsiLtrWpl8EJiPSuBy
+ocAJncc5XcHGs4SQf7TvxUjep39CwpsNmHxbeYprE8MvCvc2sbsmJLwGfwRTxigy
+9C/c0E4CmFu45BlilhQFl5P6hh30TRWGQ0B5+eAu36SIsLHre8K9tI8oFwjUoGah
+VZ64njyUOekrytdE8N29ruMcglFiueQb4TpWXIA9n5NdbPMB3zMoIPBulv2sx6xa
+OyoGlqD/HWFvg3+LAdu4BDswcJ68xChxGxqi4pJqyvjU97DcqKD5cHtzCza6CMH4
+sRG0dwhT59RI90x+WACVsRj/8H8mhcj0SprRIf3d9dRz
+=lDF5
+-----END PGP PUBLIC KEY BLOCK-----

test/modules/key-test.js
@@ -251,11 +251,17 @@ describe('Key unit test', () => {
   });
 
   describe('filterUserIdsByEmail', () => {
-    it('should return public key from private key', async () => {
+    it('should filter user by e-mail', async () => {
       const {keys: [key]} = await openpgp.key.readArmored(testKeys.maditab_pub);
       const {users} = filterUserIdsByEmail(key, 'madita@mailvelope.com');
       expect(users.length).to.equal(1);
       expect(users[0].userId.name).to.equal('Madita Bernstone');
+    });
+    it('should filter out user attributes', async () => {
+      const {keys: [key]} = await openpgp.key.readArmored(testKeys.wiktor_pub);
+      const {users} = filterUserIdsByEmail(key, 'wiktor@metacode.biz');
+      expect(users.length).to.equal(1);
+      expect(users[0].userId.email).to.equal('wiktor@metacode.biz');
     });
   });
 });