d64fc3965bd4cbf3350e2cdca8d2b3bbc21951b2
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2020-09-15 13:35:28 +0200

    wkd: Add missing `l` parameter to WKD queries
    The latest version of the WKD spec [0] specifies an `l` parameter that can
    be used by providers to select a key from a database.
    
    This change adds the missing parameter to queries done by Mailvelope.
    
    [0]: https://datatracker.ietf.org/doc/html/draft-koch-openpgp-webkey-service-10#section-3.1

--- test/modules/wkdLocate-test.js
+++ test/modules/wkdLocate-test.js
@@ -5,7 +5,7 @@ describe('WKD unit test', () => {
   describe('buildWKDUrl', () => {
     it('should generate a WKD URL', async () => {
       const wkdURL = await buildWKDUrl('demo@mailvelope.com');
-      expect(wkdURL).to.equal('https://mailvelope.com/.well-known/openpgpkey/hu/t81jm3hwduh6edujodewwfi9ye6c4urt');
+      expect(wkdURL).to.equal('https://mailvelope.com/.well-known/openpgpkey/hu/t81jm3hwduh6edujodewwfi9ye6c4urt?l=demo');
     });
   });
 });
