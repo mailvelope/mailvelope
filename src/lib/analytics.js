8074a1300e4cd135a8dbfa8006b656382697af07
John Hess <john@jthess.com>
2024-03-13 13:08:38 -0500

    measure time elapsed since last step in onboarding process

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -3,7 +3,7 @@
  * Licensed under the GNU Affero General Public License version 3
  */
 
-import {CleanInsights} from 'clean-insights-sdk';
+import {BrowserStore, CleanInsights} from 'clean-insights-sdk';
 import {matchPattern2RegEx} from './util';
 import mvelo from './lib-mvelo';
 import defaults from '../res/defaults.json';
@@ -17,39 +17,60 @@ const ONBOARDING_CATEGORY = 'onboarding';
 export const LOAD_EXTENSION = 'Load Extension';
 export const ADD_KEY = 'Added Key';
 export const COMMUNIATION = 'Communication';
-// const ONBOARDING_STEPS = [
-//   LOAD_EXTENSION,
-//   ADD_KEY,
-//   COMMUNIATION,
-// ];
+const ONBOARDING_STEPS = [
+  LOAD_EXTENSION,
+  ADD_KEY,
+  COMMUNIATION,
+];
+
 // DO NOT DEPLOY: Change to 1% for deployement.
 export const PERCENT_OF_ONBOARDERS_TO_PROMPT = 100;
 
-export const ci = new CleanInsights({
-  'server': 'https://metrics.cleaninsights.org/cleaninsights.php',
-  // DO NOT DEPLOY: Testing ID
-  'siteId': 37,  // Mailvelope's site ID on Clean Insights Matomo instance.
-  'persistEveryNTimes': 1,
-  'debug': true, // DO NOT DEPLOY: Debug off for deployment.
-  'campaigns': {
-    [PROVIDER_CAMPAIGN]: {
-      'start': '2022-02-01T00:00:00-00:00',
-      'end': '2023-01-31T23:59:59-00:00',
-      'aggregationPeriodLength': 7, // days
-      'numberOfPeriods': 53,
-      // Record which sites are used each day.  Subsequent same-day visits aren't interesting.
-      'onlyRecordOnce': true,
-    },
-    [ONBOARDING_CAMPAIGN]: {
-      'start': '2024-03-01T00:00:00-00:00',
-      'end': '2024-12-31T23:59:59-00:00',
-      'aggregationPeriodLength': 1, // days
-      'numberOfPeriods': 30,
-      // We only care *that* a step was completed, not e.g. how many times you decrypted a message.
-      'onlyRecordOnce': true,
-    },
+// Add basic K:V storage so we can keep timestamps and deduplicate actions.
+class BrowserStoreWithKV extends BrowserStore {
+  setItem(key, value) {
+    if (!this.kv) {
+      this.kv = {};
+    }
+    this.kv[key] = value;
   }
-});
+
+  getItem(key) {
+    if (!this.kv) {
+      this.kv = {};
+    }
+    return this.kv[key];
+  }
+}
+
+const store = new BrowserStoreWithKV();
+
+export const ci = new CleanInsights(
+  {
+    'server': 'https://metrics.cleaninsights.org/cleaninsights.php',
+    // DO NOT DEPLOY: Testing ID
+    'siteId': 37,  // Mailvelope's site ID on Clean Insights Matomo instance.
+    'persistEveryNTimes': 1,
+    'debug': true, // DO NOT DEPLOY: Debug off for deployment.
+    'campaigns': {
+      [PROVIDER_CAMPAIGN]: {
+        'start': '2022-02-01T00:00:00-00:00',
+        'end': '2023-01-31T23:59:59-00:00',
+        'aggregationPeriodLength': 7, // days
+        'numberOfPeriods': 53,
+        // Record which sites are used each day.  Subsequent same-day visits aren't interesting.
+        'onlyRecordOnce': true,
+      },
+      [ONBOARDING_CAMPAIGN]: {
+        'start': '2024-03-01T00:00:00-00:00',
+        'end': '2024-12-31T23:59:59-00:00',
+        'aggregationPeriodLength': 1, // days
+        'numberOfPeriods': 30,
+      },
+    }
+  },
+  store
+);
 
 /* Record a hit to a domain in the watchList (i.e. where the mvelo content script was injected).
  *
@@ -74,7 +95,33 @@ export function measureWatchListHit(url) {
   }
 }
 
+/* Record that an onboarding step was completed and how long it's been since the first time the
+ * previous step was completed.
+ *
+ * Pre-requisite actions are listed in ONBOARDING_STEPS.  Names capture the specific mechanism
+ * used to perform the action e.g. "Generate" or "Import".
+ */
 export function recordOnboardingStep(action, name) {
-  console.log('Recording onboarding step:', ONBOARDING_CATEGORY, action, ONBOARDING_CAMPAIGN, name);
-  ci.measureEvent(ONBOARDING_CATEGORY, action, ONBOARDING_CAMPAIGN, name);
+  const this_action_already_recorded = store.getItem(action) !== undefined;
+  const this_step_performed_at = Date.now();
+  if (!this_action_already_recorded) {
+    // Save the timestamp of the first time this action was performed.
+    store.setItem(action, this_step_performed_at);
+  }
+  const last_step_timestamp = store.getItem(ONBOARDING_STEPS[ONBOARDING_STEPS.indexOf(action) - 1]);
+  let elapsed = null;
+  if (last_step_timestamp) {
+    elapsed = (this_step_performed_at - last_step_timestamp) / 1000;  // Report seconds, not milliseconds.
+  }
+  
+  // For this campaign, onlyRecordOnce isn't quite right.  We want to record once *ever*,
+  // not once per aggregation period.  So we'll use the store to deduplicate.
+  const can_tuple = [ONBOARDING_CATEGORY, action, name];
+  console.log(store.getItem(can_tuple));
+  if (!store.getItem(can_tuple)) {
+    console.log('Recording onboarding step:', ONBOARDING_CATEGORY, action, ONBOARDING_CAMPAIGN, name, elapsed);
+    ci.measureEvent(ONBOARDING_CATEGORY, action, ONBOARDING_CAMPAIGN, name, elapsed);
+    store.setItem(can_tuple, true);
+    ci.persist();
+  }
 }
