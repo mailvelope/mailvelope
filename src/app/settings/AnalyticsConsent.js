976e66ea7e433ec881b4857072fb86da21057f2d
Anderson <1megaword1@gmail.com>
2024-04-16 22:07:39 -0500

    privacy consent modal added

--- src/app/settings/AnalyticsConsent.js
+++ src/app/settings/AnalyticsConsent.js
@@ -9,6 +9,7 @@ import * as l10n from '../../lib/l10n';
 import {ONBOARDING_CAMPAIGN} from '../../lib/analytics';
 import {port} from '../app';
 import {useHistory} from 'react-router-dom';
+import Modal from '../../components/util/Modal';
 
 l10n.register([
   'analytics_interstitial_header',
@@ -26,6 +27,9 @@ l10n.register([
   'analytics_consent_interstitial_Btn_Learn_More',
   'dialog_no_button',
   'dialog_yes_button',
+  'header_analyticsconsent_modal',
+  'paragraph_analyticsconsent_modal',
+  'button_analyticsconsent_modal'
 ]);
 
 function withHistory(Component) {
@@ -40,8 +44,10 @@ class AnalyticsConsent extends React.Component {
     super(props);
     this.handleSelection = this.handleSelection.bind(this);
     this.toggleExpansion = this.toggleExpansion.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
     this.state = {
       expanded: false,
+      showModal: false,
     };
   }
 
@@ -63,9 +69,19 @@ class AnalyticsConsent extends React.Component {
     window.open('https://mailvelope.com/en/faq#analytics', '_blank');
   }
 
+  toggleModal() { 
+    this.setState(prevState => ({showModal: !prevState.showModal}));
+  }
+
   render() {
     return (
       <div className="jumbotron">
+        <Modal className="text-center mt-5" isOpen={this.state.showModal} onHide={this.toggleModal} hideHeader={true} hideFooter={true}>
+          <img className="img-fluid my-4" src="/img/ic_thankyou.svg" />
+          <h2 className="">{l10n.map.header_analyticsconsent_modal}</h2>
+          <p className="">{l10n.map.paragraph_analyticsconsent_modal}</p>
+          <button className="btn btn-consent mb-4 " type="button" data-selection="yes" onClick={this.handleSelection}>{l10n.map.button_analyticsconsent_modal}</button>
+        </Modal>
         <section className="card text-center">
           <div className="container">
             <div className="row justify-content-center">
@@ -83,7 +99,7 @@ class AnalyticsConsent extends React.Component {
                 <p className="text-center w-50 mx-2">{l10n.map.analytics_consent_interstitial_message}</p>
               </div>
               <button className="btn btn-consent mx-2" type="button" data-selection="no" onClick={this.handleSelection}>{l10n.map.dialog_no_btn}</button>
-              <button className="btn btn-consent mx-2" type="button" data-selection="yes" onClick={this.handleSelection}>{l10n.map.dialog_yes_btn}</button>
+              <button className="btn btn-consent mx-2" type="button" data-selection="yes" onClick={this.toggleModal}>{l10n.map.dialog_yes_btn}</button>
             </div>
           </div>
           <p className="accordion-control my-4" onClick={this.toggleExpansion}>{l10n.map.analytics_consent_interstitial_learn_more} <span className="collapse-icon"><img className="img-fluid" src="/img/arrow.svg" /></span></p>
@@ -108,7 +124,7 @@ class AnalyticsConsent extends React.Component {
               </div>
               <div className="col-md-5 text-left">
                 <p className="font-weight-bold  mb-0">{l10n.map.analytics_consent_interstitial_Your_Privacy_is_Preserved_Title}</p>
-                <p className="">{l10n.map.analytics_consent_interstitial_Your_Privacy_is_Preserved_Pt1} <a href='https://cleaninsights.org/'>Clean Insights</a> {l10n.map.analytics_consent_interstitial_Your_Privacy_is_Preserved_Pt2}</p>
+                <p className="">{l10n.map.analytics_consent_interstitial_Your_Privacy_is_Preserved_Pt1} <a href="https://cleaninsights.org/">Clean Insights</a> {l10n.map.analytics_consent_interstitial_Your_Privacy_is_Preserved_Pt2}</p>
               </div>
               <div className="col-md-1">
                 <span className="collapse-icon"><img className="img-fluid w-100" src="/img/faqInfo.svg" /></span>
