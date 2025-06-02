f8353d1f628954c46478d84fcb32290df2ca94c0
Igor Ianishevskyi <iianishevskyi@proton.me>
2025-06-02 19:23:25 +0200

    Remove redundant comment on backup modal visibility in GenerateKey component

--- src/app/keyring/GenerateKey.js
+++ src/app/keyring/GenerateKey.js
@@ -65,7 +65,7 @@ export default class GenerateKey extends React.Component {
       errors: {}, // form errors
       key: null, // generated key
       modified: false,
-      backupModalVisible: false, // backup modal visibility
+      backupModalVisible: false
     };
   }
 
